407358d9863aa3a60302cc8f069d1e377d66cc33
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2020-05-12 21:26:40 +0900

    [fix]rails 課題3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_084406) do
+ActiveRecord::Schema.define(version: 2020_05_12_120920) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 2020_05_06_084406) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "img"
-    t.string "profile_image_id"
     t.string "introduction"
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
